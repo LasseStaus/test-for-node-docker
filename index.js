d2165513813b21e8e0faf6da8646190101ef9236
Lasse Stausgaard <L.stausgaard@gmail.com>
2021-10-18 22:24:48 +0200

    testing

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
+
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -20,10 +23,14 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi there!!!</h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.PORT || 8080;
 
